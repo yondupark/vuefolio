9b9f4c0c584ea309ddaf3a8353198c621e367eb4
SangWanPark <parksw8218@gmail.com>
2022-02-10 14:20:59 +0900

    에디터 수정

--- src/components/Board/boardWrite.vue
+++ src/components/Board/boardWrite.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="writer">
-    <Tiptap />
+    <Tiptap height="300" />
   </div>
 </template>
 
@@ -17,7 +17,13 @@ export default {
 
 <style>
 #writer {
+  width: 100rem;
+  margin: 0 auto;
   min-height: 95vh;
   padding: 20rem 0;
+}
+.ProseMirror {
+  height: 50rem;
+  overflow: auto;
 }
 </style>
--- src/components/Board/tiptap.vue
+++ src/components/Board/tiptap.vue
@@ -1,25 +1,97 @@
 <template>
+  <div v-if="editor">
+    <bubble-menu
+      class="bubble-menu"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+    >
+      <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+        Bold
+      </button>
+      <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+        Italic
+      </button>
+      <button @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+        Strike
+      </button>
+    </bubble-menu>
+
+    <floating-menu
+      class="floating-menu"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+    >
+      <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+        H1
+      </button>
+      <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+        H2
+      </button>
+      <button @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
+        Bullet List
+      </button>
+    </floating-menu>
+  </div>
+
   <editor-content :editor="editor" />
 </template>
 
 <script>
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+import {
+  Editor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from '@tiptap/vue-3'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Image from '@tiptap/extension-image'
+import Dropcursor from '@tiptap/extension-dropcursor'
 import StarterKit from '@tiptap/starter-kit'
 
 export default {
   components: {
     EditorContent,
+    BubbleMenu,
+    FloatingMenu,
   },
 
-  setup() {
-    const editor = useEditor({
-      content: '<p>이것은 에디터 영역인것이여 🎉. 이제 여기다가 작성을 허믄 db로 갔다가 상세에서 뿌려질것이여</p>',
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  methods: {
+    addImage() {
+      const url = window.prompt('URL')
+
+      if (url) {
+        this.editor.chain().focus().setImage({ src: url }).run()
+      }
+    },
+  },
+  mounted() {
+    this.editor = new Editor({
       extensions: [
         StarterKit,
+        Document,
+        Paragraph,
+        Text,
+        Image,
+        Dropcursor,
       ],
+      content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://source.unsplash.com/8xznAGy4HcY/800x400" />
+        <img src="https://source.unsplash.com/K9QHL52rE2k/800x400" />
+      `,
     })
+  },
 
-    return { editor }
+  beforeUnmount() {
+    this.editor.destroy()
   },
 }
 </script>
